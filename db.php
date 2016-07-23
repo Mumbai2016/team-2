6d1d0571bfd9ff65cabd65e2f8266b478bf4d394
Deepak Tiwari <deodaardeepak@gmail.com>
2016-07-24 04:05:56 +0530

    Changed credentials to deploy project on server

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect('localhost','root','hello123world456','atma') or die(mysql_error());
-$database = mysqli_select_db($con,'u414877475_cache') or die(mysql_error());
+$con = mysqli_connect('localhost','root','root','atma') or die(mysql_error());
+$database = mysqli_select_db($con,'atma') or die(mysql_error());
 ?>
