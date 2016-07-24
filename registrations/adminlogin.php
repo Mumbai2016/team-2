28434ce6e2677c907bc3c3bc9d027e3659ba7c06
Malabika Sen <malabika1310@gmail.com>
2016-07-24 07:54:40 +0530

    added sessions to adminlogin.php

--- registrations/adminlogin.php
+++ registrations/adminlogin.php
@@ -20,6 +20,16 @@ session_start();
       <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
     <link href="../css/styles.css" rel="stylesheet">
+    <?php
+   session_start(); //starts the session
+   if($_SESSION['Username']){ // checks if the user is logged in  
+   }
+   else{
+      header("location: ../registrations/login.php"); // redirects if user is not logged in
+   }
+   $user = $_SESSION['Username']; //assigns user value
+   ?>
+
 
  
 
